042f1c76c3a25d829404a89ef6da5a10aa7fe68a
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-05-29 08:42:23 +1000

    ye

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Animator anim;
+
+    void Update()
+    {
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        //nim.SetFloat("Speed", Input.GetKey(KeyCode.));
+    }
+}
